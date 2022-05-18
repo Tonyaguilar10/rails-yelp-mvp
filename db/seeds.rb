1b36784b01b08c4b60e48c5d6204d46bbf138ea6
jose aguilar <jose.aguilar.miranda@gmail.com>
2022-05-18 15:03:08 +0200

    2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save!
+  2.times do
+    Review.create(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5),
+      restaurant_id: restaurant.id
+    )
+  end
+end
